54efd2eebbbfea29a7c6c13819dd20081c830656
андрей максименко <mentos93@yandex.ru>
2025-10-31 19:19:31 +0400

    checked by lints

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_of_dicts: list[dict], needed_state='EXECUTED') -> list[dict]:
+def filter_by_state(list_of_dicts: list[dict], needed_state: str = "EXECUTED") -> list[dict]:
     """Функция, принимающая список словарей и возвращающая новый список словарей,
     состоящий из словарей старого списка, у которых параметр state соответствует полученному
     параметру"""
@@ -6,18 +6,17 @@ def filter_by_state(list_of_dicts: list[dict], needed_state='EXECUTED') -> list[
     list_for_result = []
 
     for dictio in list_of_dicts:
-        if dictio['state'] == needed_state:
+        if dictio["state"] == needed_state:
             list_for_result.append(dictio)
 
     return list_for_result
 
 
-def sort_by_date(list_of_dicts: list[dict], order=True) -> list[dict]:
+def sort_by_date(list_of_dicts: list[dict], order: bool = True) -> list[dict]:
     """Функция, принимающая список словарей и возвращающая новый список словарей,
     состоящий из словарей старого списка, отсортированный по убыванию даты
     (если второй параметр - True и по возрастанию, если - False)"""
 
-    sorted_list = sorted(list_of_dicts, key=lambda d: d['date'], reverse=order)
+    sorted_list = sorted(list_of_dicts, key=lambda d: d["date"], reverse=order)
 
     return sorted_list
-
