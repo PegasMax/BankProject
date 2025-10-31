c938c301614975c5bd13551f00b47080d32ca124
андрей максименко <mentos93@yandex.ru>
2025-10-31 19:14:01 +0400

    added function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,13 @@ def filter_by_state(list_of_dicts: list[dict], needed_state='EXECUTED') -> list[
 
     return list_for_result
 
+
+def sort_by_date(list_of_dicts: list[dict], order=True) -> list[dict]:
+    """Функция, принимающая список словарей и возвращающая новый список словарей,
+    состоящий из словарей старого списка, отсортированный по убыванию даты
+    (если второй параметр - True и по возрастанию, если - False)"""
+
+    sorted_list = sorted(list_of_dicts, key=lambda d: d['date'], reverse=order)
+
+    return sorted_list
+
