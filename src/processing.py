f14b25dfafeebc2bce86b05b0c9070c0c0e00ff3
андрей максименко <mentos93@yandex.ru>
2025-10-31 20:09:49 +0400

    added README

--- src/processing.py
+++ src/processing.py
@@ -15,7 +15,7 @@ def filter_by_state(list_of_dicts: list[dict], needed_state: str = "EXECUTED") -
 def sort_by_date(list_of_dicts: list[dict], order: bool = True) -> list[dict]:
     """Функция, принимающая список словарей и возвращающая новый список словарей,
     состоящий из словарей старого списка, отсортированный по убыванию даты
-    (если второй параметр - True и по возрастанию, если - False)"""
+    (если второй параметр не задан или True и по возрастанию, если - False)"""
 
     sorted_list = sorted(list_of_dicts, key=lambda d: d["date"], reverse=order)
 
