fdbfc3c3ab9a148d0fdf2b83bb412f47823a6e2e
андрей максименко <mentos93@yandex.ru>
2025-11-11 22:24:50 +0400

    modified by black

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,7 @@
-def filter_by_currency(transactions: list[dict], currency:str) -> iter(dict):
+from typing import Generator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Generator[dict]:
     """Принимает на вход список словарей, представляющих транзакции. Возвращает итератор,
     который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)"""
 
@@ -7,14 +10,14 @@ def filter_by_currency(transactions: list[dict], currency:str) -> iter(dict):
             yield transaction
 
 
-def transaction_descriptions(transactions: list[dict]) -> iter(str) :
+def transaction_descriptions(transactions: list[dict]) -> Generator[str]:
     """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
 
     for transaction in transactions:
         yield transaction["description"]
 
 
-def card_number_generator(start_number, finish_number) -> iter(str):
+def card_number_generator(start_number: int, finish_number: int) -> Generator[str]:
     """Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX,
     где X — цифра номера карты.
     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
@@ -24,13 +27,13 @@ def card_number_generator(start_number, finish_number) -> iter(str):
         number_str = str(number)
 
         # При необходимости добавляем нолики в начало номера карты
-        result_str = "0" * (16-len(number_str)) + number_str
+        result_str = "0" * (16 - len(number_str)) + number_str
         result_with_spaces = ""
 
         # Дополняем номер карты разделителями
         for i, x in enumerate(result_str):
             result_with_spaces += x
-            if (i + 1) % 4 == 0 and i + 1 !=16:
+            if (i + 1) % 4 == 0 and i + 1 != 16:
                 result_with_spaces += " "
 
         yield result_with_spaces
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,18 +1,22 @@
-from collections.abc import generator
-
 import pytest
 
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 from tests.conftest import call_transactions_list
 
-
 # Тестирование функции filter_by_currency
+
 
 def test_filter_by_currency_basic(call_transactions_list: dict):
     """Проверка базового функционала"""
-    assert list(filter_by_currency(call_transactions_list["all_transactions"], "USD")) == call_transactions_list["USD_transactions"]
+    assert (
+        list(filter_by_currency(call_transactions_list["all_transactions"], "USD"))
+        == call_transactions_list["USD_transactions"]
+    )
 
-    assert list(filter_by_currency(call_transactions_list["all_transactions"], "RUB")) == call_transactions_list["RUB_transactions"]
+    assert (
+        list(filter_by_currency(call_transactions_list["all_transactions"], "RUB"))
+        == call_transactions_list["RUB_transactions"]
+    )
 
 
 def test_filter_by_currency_upset(call_transactions_list: dict):
@@ -27,9 +31,13 @@ def test_filter_by_currency_empty_list():
 
 # Тестирование функции transaction_descriptions
 
+
 def test_transaction_descriptions_basic(call_transactions_list: dict):
     """Проверка на то, что функция возвращает корректные описания для каждой транзакции"""
-    assert list(transaction_descriptions(call_transactions_list["all_transactions"])) == call_transactions_list["all_transactions_descriptions"]
+    assert (
+        list(transaction_descriptions(call_transactions_list["all_transactions"]))
+        == call_transactions_list["all_transactions_descriptions"]
+    )
 
 
 def test_transaction_descriptions_empty():
@@ -39,37 +47,41 @@ def test_transaction_descriptions_empty():
 
 # Тестирование функции card_number_generator
 
+
 @pytest.mark.parametrize(
     "start_number, finish_number, expected_result",
-        [(2200404533221000,
-        2200404533221010,
-        ["2200 4045 3322 1000", "2200 4045 3322 1001", "2200 4045 3322 1002", "2200 4045 3322 1003", "2200 4045 3322 1004", "2200 4045 3322 1005", "2200 4045 3322 1006",
-         "2200 4045 3322 1007", "2200 4045 3322 1008", "2200 4045 3322 1009", "2200 4045 3322 1010"]
-        ),
+    [
         (
-        1,
-        2,
-        ["0000 0000 0000 0001", "0000 0000 0000 0002"]
-        )]
+            2200404533221000,
+            2200404533221010,
+            [
+                "2200 4045 3322 1000",
+                "2200 4045 3322 1001",
+                "2200 4045 3322 1002",
+                "2200 4045 3322 1003",
+                "2200 4045 3322 1004",
+                "2200 4045 3322 1005",
+                "2200 4045 3322 1006",
+                "2200 4045 3322 1007",
+                "2200 4045 3322 1008",
+                "2200 4045 3322 1009",
+                "2200 4045 3322 1010",
+            ],
+        ),
+        (1, 2, ["0000 0000 0000 0001", "0000 0000 0000 0002"]),
+    ],
 )
 def test_card_number_generator_basic(start_number, finish_number, expected_result):
     """Проверка выдачи номеров в заданном диапазоне и корректности форматирования номеров карт"""
-    assert list(card_number_generator(start_number,finish_number)) == expected_result
+    assert list(card_number_generator(start_number, finish_number)) == expected_result
 
 
 @pytest.mark.parametrize(
     "start_number, finish_number, expected_first, expected_last",
-        [(2200404533221000,
-        2200404533221010,
-        "2200 4045 3322 1000",
-        "2200 4045 3322 1010"
-        ),
-        (
-        1,
-        1000000,
-        "0000 0000 0000 0001",
-        "0000 0000 0100 0000"
-        )]
+    [
+        (2200404533221000, 2200404533221010, "2200 4045 3322 1000", "2200 4045 3322 1010"),
+        (1, 1000000, "0000 0000 0000 0001", "0000 0000 0100 0000"),
+    ],
 )
 def test_card_number_generator_extreme(start_number, finish_number, expected_first, expected_last):
     """Проверка корректности крайних значений диапазона"""
