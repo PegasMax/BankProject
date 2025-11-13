677a4375d8e259f4f3c883b13997374f8ae4f5a6
андрей максименко <mentos93@yandex.ru>
2025-11-13 23:13:51 +0400

    new decorator log is written

---
+++ src/decorators.py
@@ -0,0 +1,38 @@
+from functools import wraps
+
+
+def print_to_file_or_console(text: str, filename: str = "") -> None:
+    if filename:
+        with open(filename, "a") as file:
+            file.write(text + "\n")
+    else:
+        print(text)
+
+
+def log(filename: str = ""):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs) -> object:
+
+            if filename:
+                file = open(filename, "w")
+                file.close()
+
+            print_to_file_or_console(f"start of executing {func.__name__}", filename)
+
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                err_type = str(type(e))
+                text = f"{func.__name__} error: {err_type}. Inputs: {args}, {kwargs}"
+                print_to_file_or_console(text, filename)
+            else:
+                text = f"{func.__name__} result = {result}"
+                print_to_file_or_console(text, filename)
+
+            print_to_file_or_console(f"finish of executing {func.__name__}", filename)
+            return func
+
+        return inner
+
+    return wrapper
