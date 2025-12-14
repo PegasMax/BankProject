133e9369aa03dac0782806a2cc8d29350c5ef883
андрей максименко <mentos93@yandex.ru>
2025-12-14 10:26:16 +0400

    added logging in utils.py and masks.py

--- src/external_api.py
+++ src/external_api.py
@@ -13,13 +13,13 @@ def convert_amount_to_rub(transaction: dict) -> float:
     amount = transaction["operationAmount"]["amount"]
     source = transaction["operationAmount"]["currency"]["code"]
 
-    url = f'https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={source}&amount={amount}'
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={source}&amount={amount}"
 
     payload = {}
 
     headers = {"apikey": API_KEY}
 
-    response = requests.request("GET",url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
     if response.status_code == 200:
         result_json = json.loads(response.text)
@@ -30,18 +30,12 @@ def convert_amount_to_rub(transaction: dict) -> float:
 
 
 usd_transaction = {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        }
+    "id": 939719570,
+    "state": "EXECUTED",
+    "date": "2018-06-30T02:08:58.425572",
+    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+    "description": "Перевод организации",
+    "from": "Счет 75106830613657916952",
+    "to": "Счет 11776614605963066702",
+}
 print(convert_amount_to_rub(usd_transaction))
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 import json
+import logging
 
 from src.external_api import convert_amount_to_rub
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("logs/utils.log")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_transactions_from_json_file(path: str) -> list[dict]:
@@ -8,6 +16,7 @@ def get_transactions_from_json_file(path: str) -> list[dict]:
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список
     """
     result_list = None
+    logger.info("Начинает работу get_transactions_from_json_file.")
 
     # Проверки и исключения
     try:
@@ -16,13 +25,17 @@ def get_transactions_from_json_file(path: str) -> list[dict]:
                 result_list = json.load(file)
                 print(result_list[1])
             except json.JSONDecodeError:
+                logger.error("Ошибка декодирования JSON формата.")
                 return []
     except FileNotFoundError:
+        logger.error(f"Ошибка отсутствия файла {path}.")
         return []
 
     if type(result_list) is not list:
+        logger.error("Ошибка: файл содержит не список.")
         return []
 
+    logger.info("Список словарей успешно получен из JSON-файла, функция прекращает работу.")
     return result_list
 
 
@@ -30,13 +43,18 @@ def get_transaction_amount_rub(transaction: dict) -> float:
     """Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
     Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
     и конвертации суммы операции в рубли"""
-
+    logger.info("Начинает работу get_transaction_amount_rub.")
     result = float(transaction["operationAmount"]["amount"])
 
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
-        return round(float(result), 2)
+        result_amount = round(float(result), 2)
+        logger.info("Транзакция рублевая, сумма была округлена, функция завершает работу.")
+        return result_amount
     elif transaction["operationAmount"]["currency"]["code"] in ["USD", "EUR"]:
         result = convert_amount_to_rub(transaction)
-        return round(result, 2)
+        result_amount = round(result, 2)
+        logger.info("Транзакция в долларах или евро, сумма была преобразована, функция завершает работу.")
+        return result_amount
     else:
+        logger.info("Транзакция не содержит допустимую валюту, Функция завершена, возвращен ноль.")
         return 0
