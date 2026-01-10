003e7a871fc7dea1c5d75ce2590e3397694fe66e
андрей максименко <mentos93@yandex.ru>
2026-01-11 00:05:16 +0400

    added analytics.py

--- src/analytics.py
+++ src/analytics.py
@@ -4,6 +4,7 @@ import re
 def process_bank_search(data:list[dict], search:str)->list[dict]:
     """Принимает список словарей с данными о банковских операциях и строку поиска, 
     возвращает список словарей, у которых в описании есть данная строка."""
+    pattern = fr'.*{search},*'
     result_list = [operation for operation in data if re.search(search,operation['description'],flags=re.IGNORECASE)]
     return result_list
 
@@ -11,8 +12,6 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
 def process_bank_operations(data:list[dict], categories:list)->dict:
     """Принимает список словарей с данными о банковских операциях и список категорий операций, а возвращает словарь,
     в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
-    categories_counted = collections.defaultdict(dict)
-    for categorie in categories:
-        categories_counted[categorie] = len(process_bank_search(data,categorie))
+    categories_counted = collections.Counter([transaction["description"] for transaction in data if transaction["description"] in categories])
 
     return categories_counted
